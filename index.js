8fa64618aaec6b416a8e936f66111d76823f9628
Cupertino Luna Trejo <cuper_luna@hotmail.com>
2023-04-21 07:46:35 -0600

    Creación del servidor con express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express=require('express');
+const app=express();
 
+//Settings 
+app.set('port',process.env.PORT||3600);
+
+
+app.listen(app.get('port'),()=>{
+    console.log('Server on port: ' + app.get('port'));
+});
