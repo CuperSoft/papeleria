e6385eac7d4530b3b33af0c6d2303ecf434d192f
Cupertino Luna Trejo <cuper_luna@hotmail.com>
2023-04-25 09:07:01 -0600

    Agregando rutas

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express=require('express');
+const morgan=require('morgan');
 const app=express();
 
 //Settings 
 app.set('port',process.env.PORT||3600);
 
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.get("/",(req,res)=>{
+   console.log(req.body);
+   res.send("Petición recibida...");
+})
 
 app.listen(app.get('port'),()=>{
     console.log('Server on port: ' + app.get('port'));
